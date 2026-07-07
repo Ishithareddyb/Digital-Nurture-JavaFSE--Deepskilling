554688ac1abcc6864715e93671a6679b4fb85cfc
Ishithareddyb <165763558+Ishithareddyb@users.noreply.github.com>
2026-07-07 23:30:18 +0530

    Update CalculatorTest.java

--- Module 4 - Test driven development and Logging framework/1. JUnit_Basic Testing Exercises/src/test/java/com/example/CalculatorTest.java
+++ Module 4 - Test driven development and Logging framework/1. JUnit_Basic Testing Exercises/src/test/java/com/example/CalculatorTest.java
@@ -1,1 +1,15 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+
+        Calculator c = new Calculator();
+
+        assertEquals(5, c.add(2, 3));
+    }
+}
