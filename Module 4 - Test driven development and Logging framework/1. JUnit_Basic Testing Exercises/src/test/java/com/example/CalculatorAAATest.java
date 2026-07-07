9567fbb7e597db2079468ba7ad65d9f514dd3f52
Ishithareddyb <165763558+Ishithareddyb@users.noreply.github.com>
2026-07-07 23:30:52 +0530

    Update CalculatorAAATest.java

--- Module 4 - Test driven development and Logging framework/1. JUnit_Basic Testing Exercises/src/test/java/com/example/CalculatorAAATest.java
+++ Module 4 - Test driven development and Logging framework/1. JUnit_Basic Testing Exercises/src/test/java/com/example/CalculatorAAATest.java
@@ -1,1 +1,37 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorAAATest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        System.out.println("Before Each Test");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After Each Test");
+    }
+
+    @Test
+    void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
