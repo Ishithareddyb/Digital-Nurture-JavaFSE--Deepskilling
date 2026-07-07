6e31a5f2516defa81c7c267270b15988240dcf76
Ishithareddyb <165763558+Ishithareddyb@users.noreply.github.com>
2026-07-07 23:57:51 +0530

    Update MyServiceTest.java

--- Module 4 - Test driven development and Logging framework/3. Mockito exercises/MockitoDemo/src/test/java/com/example/MyServiceTest.java
+++ Module 4 - Test driven development and Logging framework/3. Mockito exercises/MockitoDemo/src/test/java/com/example/MyServiceTest.java
@@ -1,1 +1,31 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        // Create Mock Object
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+
+        // Stub the method
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        // Inject Mock into Service
+        MyService service = new MyService(mockApi);
+
+        // Call method
+        String result = service.fetchData();
+
+        // Verify Result
+        assertEquals("Mock Data", result);
+
+    }
+
+}
