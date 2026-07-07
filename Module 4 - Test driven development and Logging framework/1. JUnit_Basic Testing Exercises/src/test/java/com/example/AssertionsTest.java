60767e8d2bd2fb0b5fe8741e63efa931f1b232b1
Ishithareddyb <165763558+Ishithareddyb@users.noreply.github.com>
2026-07-07 23:31:18 +0530

    Update AssertionsTest.java

--- Module 4 - Test driven development and Logging framework/1. JUnit_Basic Testing Exercises/src/test/java/com/example/AssertionsTest.java
+++ Module 4 - Test driven development and Logging framework/1. JUnit_Basic Testing Exercises/src/test/java/com/example/AssertionsTest.java
@@ -1,1 +1,28 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AssertionsTest {
+
+    @Test
+    void testAssertions() {
+
+        // Check if values are equal
+        assertEquals(5, 2 + 3);
+
+        // Check if condition is true
+        assertTrue(10 > 5);
+
+        // Check if condition is false
+        assertFalse(5 > 10);
+
+        // Check if value is null
+        String name = null;
+        assertNull(name);
+
+        // Check if object is not null
+        Object obj = new Object();
+        assertNotNull(obj);
+    }
+}
